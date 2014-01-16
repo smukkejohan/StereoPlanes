0f6698b6a4ce5552aa70ac351816ea89463f8406
Bernardo Schorr <bernardo.schorr@gmail.com>
2014-01-16 17:40:55 -0500

    added changes to boxFloor

--- src/BoxFloor.cpp
+++ src/BoxFloor.cpp
@@ -10,13 +10,20 @@
 
 void BoxFloor::setup(ofParameterGroup * params) {
     
+    params->add(surfaceHeight.set("Surface Height", 0.0, -2.0, 2.0));
     params->add(waveHeight.set("Boxes Height", 0.5, 0, 1));
     params->add(speed.set("Boxes Speed", 0.0003, 0, 0.001));
     params->add(plateauHeight.set("Plateau Height", 0.5, 0, 1));
     params->add(lightHeight.set("Light Height", -0.3, -1.0, 0));
+    params->add(distance.set("Distance", 0.2, 0, 0.6));
+    params->add(bRotation.set("Rotation", false, false, true));
     
     light.setPointLight();
     light.setPosition(0, 0, lightHeight);
+    
+    for (int i = 0; i < 400; i++) {
+        rotation.push_back(ofRandom(90));
+    }
     
 }
 
@@ -42,11 +49,15 @@ void BoxFloor::draw( ofVec2f dancerPos ) {
             
             float tempNoise = ofNoise(x + ofGetElapsedTimeMillis()*speed, y);
             
-            if (ofDist(dancerPos.x, dancerPos.y, x, y) > 0.2) {
-                ofDrawBox(x, y, 0, 0.09, 0.09, waveHeight*tempNoise);
-            } else {
-                ofDrawBox(x, y, 0, 0.09, 0.09, plateauHeight);
-            }
+            ofPushMatrix();
+            ofTranslate(x, y, 0);
+            if (bRotation)ofRotateZ (rotation[((x+1) * 10 * 20) + ((y+1) * 10)]);
+                if (ofDist(dancerPos.x, dancerPos.y, x, y) > distance) {
+                    ofDrawBox(0, 0, surfaceHeight, 0.09, 0.09, waveHeight*tempNoise);
+                } else {
+                    ofDrawBox(0, 0, surfaceHeight, 0.09, 0.09, plateauHeight);
+                }
+            ofPopMatrix();
         }
     }
     
