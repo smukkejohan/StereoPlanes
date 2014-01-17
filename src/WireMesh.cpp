d7fdb6235c36f8da68584fe7627e1fc1614ff87c
mariapaulasaba <msd403@nyu.edu>
2014-01-17 16:55:34 -0500

    Mesh Class

---
+++ src/WireMesh.cpp
@@ -0,0 +1,9 @@
+//
+//  WireMesh.cpp
+//  StereoPlanes
+//
+//  Created by Maria Paula Saba dos Reis on 1/17/14.
+//
+//
+
+#include "WireMesh.h"
---
+++ src/WireMesh.h
@@ -0,0 +1,14 @@
+//
+//  WireMesh.h
+//  StereoPlanes
+//
+//  Created by Maria Paula Saba dos Reis on 1/17/14.
+//
+//
+
+#ifndef __StereoPlanes__WireMesh__
+#define __StereoPlanes__WireMesh__
+
+#include <iostream>
+
+#endif /* defined(__StereoPlanes__WireMesh__) */
