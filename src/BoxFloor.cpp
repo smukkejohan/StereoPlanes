78cbce7d3060632189a9b19cbb6cb8e8d67b61b6
Bernardo Schorr <bernardo.schorr@gmail.com>
2014-01-22 14:47:49 -0500

    box floor working again

--- src/BoxFloor.cpp
+++ src/BoxFloor.cpp
@@ -37,25 +37,45 @@ void BoxFloor::update() {
 
 void BoxFloor::draw( int _surfaceId ) {
     
-    boxLight.enable();
+    if (_surfaceId == 0) {
     
-    for (float x = -1; x < 1; x += 0.1) {
-        for (float y = -1; y < 1; y += 0.1) {
-            
-            float tempNoise = ofNoise(x + ofGetElapsedTimeMillis()*speed, y);
-            
-            ofPushMatrix();
-            ofTranslate(x, y, 0);
-            if (bRotation)ofRotateZ (rotation[((x+1) * 10 * 20) + ((y+1) * 10)]);
-                if (ofDist(dancerPos.x, dancerPos.y, x, y) > distance) {
-                    ofDrawBox(0, 0, surfaceHeight, 0.09, 0.09, waveHeight*tempNoise);
-                } else {
-                    ofDrawBox(0, 0, surfaceHeight, 0.09, 0.09, plateauHeight);
-                }
-            ofPopMatrix();
+        boxLight.enable();
+
+        for (float x = -1; x < 1; x += 0.1) {
+            for (float y = -1; y < 1; y += 0.1) {
+                
+                float tempNoise = ofNoise(x + ofGetElapsedTimeMillis()*speed, y);
+                
+                ofPushMatrix();
+                ofTranslate(x, y, 0);
+                if (bRotation)ofRotateZ (rotation[((x+1) * 10 * 20) + ((y+1) * 10)]);
+                    if (ofDist(dancerPos.x, dancerPos.y, x, y) > platformRadius) {
+                        ofDrawBox(0, 0, surfaceHeight, 0.09, 0.09, waveHeight*tempNoise);
+                    } else {
+                        ofDrawBox(0, 0, surfaceHeight, 0.09, 0.09, plateauHeight);
+                    }
+                ofPopMatrix();
+            }
         }
+
+        boxLight.disable();
+    
     }
     
-    boxLight.disable();
+}
+
+void BoxFloor::setGui(ofxUICanvas * gui, float width){
+    ContentScene::setGui(gui, width);
+    
+    
+    gui->addSlider(indexStr+"Surface Height", -2.0, 2.0, &surfaceHeight);
+    gui->addSlider(indexStr+"Wave Height", 0.0, 1.0, &waveHeight);
+    gui->addSlider(indexStr+"Speed", 0.0, 0.001, &speed);
+    gui->addSlider(indexStr+"Plateau Height", 0.0, 1.0, &plateauHeight);
+    gui->addSlider(indexStr+"Light Position", -1.0, 0.0, &lightHeight);
+    gui->addSlider(indexStr+"Platform Radius", 0, 0.6, &platformRadius);
+    
+    gui->addToggle(indexStr+"Animate", &bRotation);
+    
     
 }
